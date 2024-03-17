34c4b879c63a16bed9c39d1e23f99783b0ebea5a
Icko <80863308+ickoxii@users.noreply.github.com>
2024-03-17 17:29:06 -0500

    improved echo printing

--- rename-branch.sh
+++ rename-branch.sh
@@ -15,10 +15,14 @@ remote="origin"
 # Rename the local branch to the new name
 echo "git branch -m $old_name $new_name"
 git branch -m "$old_name" "$new_name"
+echo "-----"
+echo ""
 
 # Delete the old branch on remote
 echo "git push $remote --delete $old_name"
 git push "$remote" --delete "$old_name"
+echo "-----"
+echo ""
 
 # Or shorter way to delete remote branch
 # git push "$remote" :"$old_name"
@@ -27,11 +31,17 @@ git push "$remote" --delete "$old_name"
 # Otherwise, git will use the old upstream name instead of <new_name>.
 echo "git branch --unset-upstream $new_name"
 git branch --unset-upstream "$new_name"
+echo "-----"
+echo ""
 
 # Push the new branch to remote
 echo "git push $remote $new_name"
 git push "$remote" "$new_name"
+echo "-----"
+echo ""
 
 # Reset the upstream branch for the new_name local branch
 echo "git push $remote -u $new_name"
 git push "$remote" -u "$new_name"
+echo "-----"
+echo ""
